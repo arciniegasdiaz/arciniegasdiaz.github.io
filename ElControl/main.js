16db23f5e0156854de51ab0c6b17579578840b13
arciniegasdiaz <60700462+arciniegasdiaz@users.noreply.github.com>
2020-11-25 12:19:14 -0500

    Actualizar Todo

--- ElControl/main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("mi-menu");
-  if (x.className === "el-menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "el-menu";
-  }
-}
